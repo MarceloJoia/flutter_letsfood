455e943c43f72cc183f7414b4cb8ff79562e484f
Marcelo Joia <sitejoia@hotmail.com>
2021-01-31 18:14:19 -0300

    Instalar o Dio e Configurar

---
+++ lib/data/network/dio_client.dart
@@ -0,0 +1,41 @@
+import 'package:dio/dio.dart';
+
+import '../../data/network/interceptors/dio_interceptor.dart';
+
+class DioClient {
+  Dio _dio;
+
+  ///Construtor
+  DioClient() {
+    _dio = dioInterceptor();
+  }
+
+  /// Metodo de requisição da API. Com o "response" já temos o retorno exato de nossa API.
+  /// Permite enviar parametros "Map<String, dynamic> queryParameters".
+  /// Method Get
+  Future<dynamic> get(String url, Map<String, dynamic> queryParameters) async {
+    try {
+      final Response response =
+          await _dio.get(url, queryParameters: queryParameters);
+      print(response);
+      return response;
+    } catch (e) {
+      print(e.toString()); // Converte a exception para String
+      throw e; // Gera a exception
+    }
+  }
+
+  /// Method Post
+  Future<dynamic> post(
+      String url, formData, Map<String, dynamic> queryParameters) async {
+    try {
+      final Response response = await _dio.post(url,
+          data: formData, queryParameters: queryParameters);
+      print(response);
+      return response;
+    } catch (e) {
+      print(e.toString()); // Converte a exception para String
+      throw e; // Gera a exception
+    }
+  }
+}
