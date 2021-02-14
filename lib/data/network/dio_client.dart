5076f5f7b90e608e187906c70ee47dba30c15676
Marcelo Joia <sitejoia@hotmail.com>
2021-02-14 10:57:52 -0300

    Preparendo Mobx

--- lib/data/network/dio_client.dart
+++ lib/data/network/dio_client.dart
@@ -1,41 +1,14 @@
 import 'package:dio/dio.dart';
 
-import '../../data/network/interceptors/dio_interceptor.dart';
-
-class DioClient {
+class DioClient () {
+  
   Dio _dio;
 
-  ///Construtor
-  DioClient() {
-    _dio = dioInterceptor();
+  DioClient () {
+    _dio = new Dio();
   }
 
-  /// Metodo de requisição da API. Com o "response" já temos o retorno exato de nossa API.
-  /// Permite enviar parametros "Map<String, dynamic> queryParameters".
-  /// Method Get
-  Future<dynamic> get(String url, Map<String, dynamic> queryParameters) async {
-    try {
-      final Response response =
-          await _dio.get(url, queryParameters: queryParameters);
-      print(response);
-      return response;
-    } catch (e) {
-      print(e.toString()); // Converte a exception para String
-      throw e; // Gera a exception
-    }
-  }
-
-  /// Method Post
-  Future<dynamic> post(
-      String url, formData, Map<String, dynamic> queryParameters) async {
-    try {
-      final Response response = await _dio.post(url,
-          data: formData, queryParameters: queryParameters);
-      print(response);
-      return response;
-    } catch (e) {
-      print(e.toString()); // Converte a exception para String
-      throw e; // Gera a exception
-    }
+  Future<dynamic> get(String url) {
+    _dio.get();
   }
 }
