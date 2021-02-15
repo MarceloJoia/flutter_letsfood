aabaff5e8bd15d68d97a292ca3dfe26220169f63
Marcelo Joia <sitejoia@hotmail.com>
2021-02-15 14:52:41 -0300

    Criar Repository e Store de Categorias

---
+++ lib/data/network/repositories/category_repository.dart
@@ -0,0 +1,27 @@
+import '../../../constants/api.dart';
+import '../dio_client.dart';
+
+class CategoryRepository {
+  var _httpClient;
+
+  // Construtor Repository
+  CategoryRepository() {
+    _httpClient = new DioClient();
+  }
+
+  // Retorna as comidas
+  /** 
+    * http://10.0.2.2/api/v1/products&token_company=tokenDaCompanhia
+    * Para isso preciso de uma Query parametro
+    */
+  Future<List<dynamic>> getCategories(String tokenCompany) async {
+    final response =
+        await _httpClient.get("${API_VERSION}/categories", queryParameters: {
+      'token_company': tokenCompany,
+    });
+    //print(response);
+
+    // Converte para lista
+    return (response.data['data'] as List).toList();
+  }
+}
---
+++ lib/stores/categories.store.dart
@@ -0,0 +1,54 @@
+import 'package:mobx/mobx.dart';
+
+import '../data/network/repositories/category_repository.dart';
+import '../../models/Category.dart';
+
+part 'categories.store.g.dart';
+
+class CategoriesStore = _CategoriesStoreBase with _$CategoriesStore;
+
+abstract class _CategoriesStoreBase with Store {
+  CategoryRepository _repository;
+
+  _CategoriesStoreBase() {
+    _repository = new CategoryRepository();
+  }
+
+  // Observa as Categorias
+  @observable
+  ObservableList<Category> categories = ObservableList<Category>();
+
+  // Obseva do pre loading
+  @observable
+  bool isLoading = false;
+
+  @action
+  void setLoading(bool value) => isLoading = value;
+
+  @action
+  void addCategory(Category category) {
+    categories.add(category);
+  }
+
+  @action
+  void removeCategory(Category category) {
+    categories.remove(category);
+  }
+
+  @action
+  void clearCategories() {
+    categories.clear();
+  }
+
+  @action
+  Future getCategories(String tokenCompany) async {
+    setLoading(true);
+    clearCategories(); // Limpar as categorias carregadas anteriormente
+    final response = await _repository.getCategories(tokenCompany);
+    setLoading(false);
+
+    response
+        .map((categoryJson) => addCategory(Category.fromJson(categoryJson)))
+        .toList();
+  }
+}
